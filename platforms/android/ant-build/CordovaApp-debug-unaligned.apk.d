cbfad6ffd033a8e2b9293a36286d76076d66eb2a
Kwun Yeung <kwun@creativeworks.com.hk>
2014-12-13 00:52:30 +0800

    Add Android platform

---
+++ platforms/android/ant-build/CordovaApp-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+/Users/kopanda/Documents/Cordova/DimsumApp/platforms/android/ant-build/CordovaApp-debug-unaligned.apk : \
+/Users/kopanda/Documents/Cordova/DimsumApp/platforms/android/ant-build/CordovaApp.ap_ \
+/Users/kopanda/Documents/Cordova/DimsumApp/platforms/android/ant-build/classes.dex \
+/Users/kopanda/Documents/Cordova/DimsumApp/platforms/android/CordovaLib/ant-build/classes.jar \
