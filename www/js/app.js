edac667bf6aed58e1dd0f6a2c480651d82bb529c
Kwun Yeung <kwun@creativeworks.com.hk>
2014-12-20 04:10:11 +0800

    add add dimsum interface

--- www/js/app.js
+++ www/js/app.js
@@ -18,5 +18,57 @@ $(document).ready(function(e){
 	
 	});
 	
+	function getPhoto(source) {
+      // Retrieve image file location from specified source
+      navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
+        destinationType: destinationType.FILE_URI,
+        sourceType: source });
+    }
+
+    function uploadPhoto() {
+
+        //selected photo URI is in the src attribute (we set this on getPhoto)
+        var imageURI = document.getElementById('smallImage').getAttribute("src");
+        if (!imageURI) {
+            alert('Please select an image first.');
+            return;
+        }
+
+        //set upload options
+        var options = new FileUploadOptions();
+        options.fileKey = "file";
+        options.fileName = imageURI.substr(imageURI.lastIndexOf('/')+1);
+        options.mimeType = "image/jpeg";
+
+        options.params = {
+            data.Dimsum.name: document.getElementById("name").value,
+            data.Dimsum.stock: document.getElementById("stock").value,
+            data.Dimsum.type_id: document.getElementById("typeId").value
+        }
+
+        var ft = new FileTransfer();
+        ft.upload(imageURI, encodeURI("http://10.0.2.2/dimsumapp/dimsums/index.json"), win, fail, options);
+    }
+
+    // Called if something bad happens.
+    //
+    function onFail(message) {
+      console.log('Failed because: ' + message);
+    }
+
+    function win(r) {
+        console.log("Code = " + r.responseCode);
+        console.log("Response = " + r.response);
+        //alert("Response =" + r.response);
+        console.log("Sent = " + r.bytesSent);
+    }
+
+    function fail(error) {
+        alert("An error has occurred: Code = " + error.code);
+        console.log("upload error source " + error.source);
+        console.log("upload error target " + error.target);
+    }
+
+ 
 	//
 });
